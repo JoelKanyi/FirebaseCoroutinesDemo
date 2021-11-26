0918368e55c491b147df19de727da6cd5f96fef1
JoelKanyi <joelkanyi98@gmail.com>
2021-11-26 21:01:02 +0300

    initial

---
+++ app/src/main/java/com/kanyideveloper/firebasecoroutinesdemo/repository/MainRepository.kt
@@ -0,0 +1,44 @@
+package com.kanyideveloper.firebasecoroutinesdemo.repository
+
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.kanyideveloper.firebasecoroutinesdemo.model.User
+import com.kanyideveloper.firebasecoroutinesdemo.util.Resource
+import com.kanyideveloper.firebasecoroutinesdemo.util.safeCall
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+
+class MainRepository {
+    private val firebaseAuth = FirebaseAuth.getInstance()
+    private val databaseReference = FirebaseDatabase.getInstance().getReference("users")
+
+    suspend fun register(
+        name: String,
+        email: String,
+        phone: String,
+        password: String
+    ): Resource<AuthResult> {
+        return withContext(Dispatchers.IO) {
+            safeCall {
+                val result = firebaseAuth.createUserWithEmailAndPassword(email, password).await()
+                firebaseAuth.currentUser?.sendEmailVerification()?.await()
+                val uid = result.user?.uid!!
+                val user = User(name, email, phone)
+                databaseReference.child(uid).setValue(user).await()
+                Resource.Success(result)
+            }
+        }
+    }
+
+    suspend fun login(email: String, password: String): Resource<AuthResult> {
+        return withContext(Dispatchers.IO) {
+            safeCall {
+                val result = firebaseAuth.signInWithEmailAndPassword(email, password).await()
+                Resource.Success(result)
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/kanyideveloper/firebasecoroutinesdemo/viewmodel/MainViewModel.kt
@@ -0,0 +1,60 @@
+package com.kanyideveloper.firebasecoroutinesdemo.viewmodel
+
+import android.app.Application
+import android.util.Patterns
+import androidx.lifecycle.*
+import com.google.firebase.auth.AuthResult
+import com.kanyideveloper.firebasecoroutinesdemo.repository.MainRepository
+import com.kanyideveloper.firebasecoroutinesdemo.util.Resource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainViewModel(
+) : ViewModel() {
+
+    private val _registerStatus = MutableLiveData<Resource<AuthResult>>()
+    val registerStatus: LiveData<Resource<AuthResult>> = _registerStatus
+
+    private val _loginStatus = MutableLiveData<Resource<AuthResult>>()
+    val loginStatus: LiveData<Resource<AuthResult>> = _loginStatus
+
+    private val repository = MainRepository()
+
+
+    fun registerUser(
+        name: String,
+        email: String,
+        phone: String,
+        password: String
+    ) {
+        var error =
+            if (email.isEmpty() || name.isEmpty() || password.isEmpty() || phone.isEmpty()) {
+                "Empty Strings"
+            } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                "Not a valid Email"
+            } else null
+
+        error?.let {
+            _registerStatus.postValue(Resource.Error(it))
+            return
+        }
+        _registerStatus.postValue(Resource.Loading())
+
+        viewModelScope.launch(Dispatchers.Main) {
+            val result = repository.register(name, email, phone, password)
+            _registerStatus.postValue(result)
+        }
+    }
+
+    fun loginUser(email: String, password: String) {
+        if (email.isEmpty() || password.isEmpty()) {
+            _loginStatus.postValue(Resource.Error("Empty Strings"))
+        } else {
+            _loginStatus.postValue(Resource.Loading())
+            viewModelScope.launch(Dispatchers.Main) {
+                val result = repository.login(email, password)
+                _loginStatus.postValue(result)
+            }
+        }
+    }
+}
